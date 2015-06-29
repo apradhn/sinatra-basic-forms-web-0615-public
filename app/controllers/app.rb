56b14839e2ee6f845e65da0f699ec17806cc9bbd
apradhn <abhijit.pradhan6@gmail.com>
2015-06-29 15:11:21 -0400

    all done

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -10,6 +10,18 @@ class App < Sinatra::Base
     @song = Song.find(params[:id])
     erb :'songs/show'
   end
+
+  get '/songs/:id/edit' do
+    @song = Song.find(params[:id])
+    erb :'songs/edit'
+  end
+
+  post '/songs/:id/edit' do
+    song = params[:song]
+    title, artist, album, genre, length = song[:title], song[:artist], song[:album], song[:genre], song[:length]
+    Song.update(params[:id], title: title, artist: artist, album: album, genre: genre, length: length)
+    redirect('/songs/' + params[:id])
+  end
   
 end
 
